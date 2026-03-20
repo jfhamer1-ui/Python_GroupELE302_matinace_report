fc1b587b4a03d12ae45953ea7862934378b17db3
jjhamer123 <jjhamer123@gmail.com>
2026-03-20 18:30:19 +0000

    Added template file to start

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+def main():
+    #main function
+    print("Starting")
+    parse_data()
+    create_HTML()
+    create_PDF()
+
+def create_HTML():
+    print("Creating HTML")
+
+def create_PDF():
+    print("Creating PDF")
+
+def parse_data():
+    print("Parsing data")
+
+def format_HTML():
+    print("Formatting HTML")
+
+def order_data():
+    print("Ordering data")
+
+def generate_downtime_instances():
+    print("Generating downtime instances")
+
+if __name__ == "__main__":
+    main()
